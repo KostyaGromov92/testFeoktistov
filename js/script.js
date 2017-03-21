6b590a937e24d7f1785bbd0512a3cffb3a548c85
DESKTOP-52VJMD4\Костя <kotya@zeoalliance.com>
2017-03-21 12:41:16 +0200

    Add slider

--- js/script.js
+++ js/script.js
@@ -22,4 +22,32 @@ $( document ).ready(function() {
     $(".burger-menu").click(function () {
         $(this).toggleClass("menu-on");
     });
+
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        nav:true,
+        dots: true,
+        touchDrag: true
+    });
+
+    // $(window).resize(function () {
+    //     var imgHeight = $('.owl-carousel .owl-item img').height();
+    //     $('.work__wrap--slider-item').height(imgHeight);
+    // });
+    //
+    // var imgHeight = $('.owl-carousel .owl-item img').height();
+    // $('.work__wrap--slider-item').height(imgHeight);
+
+    $('.owl-prev, .owl-next').text(' ');
+
+    $('.work__wrap--slider-zoom').click(function () {
+        if($('.work__wrap--slider-item img').hasClass('animate-on')) {
+            $('.work__wrap--slider-item img').removeClass('animate-on').addClass('animate-off');
+            $('.owl-nav').css('display','block');
+        } else {
+            $('.work__wrap--slider-item img').removeClass('animate-off').addClass('animate-on');
+            $('.owl-nav').css('display','none');
+        }
+    });
+
 });
